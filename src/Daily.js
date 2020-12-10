5809292bc0cb8271218a4ec4ba12dfac78dd829a
gabo995 <gabelittleorange@gmail.com>
2020-12-09 20:34:04 -0600

    added acordion section as placeholder

--- src/Daily.js
+++ src/Daily.js
@@ -1,30 +1,57 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useFetch } from "./useFetch";
 import { iconKey } from "./WeatherCard";
 const Daily = ({ city, latitude, longitude }) => {
+  const [show, setShow] = useState(true);
+  const [position, setPosition] = useState(0);
   const base = "https://api.openweathermap.org/data/2.5/onecall";
   const key = "05e47cb6f8fc8afa437fc32af1218b36";
   const query = `?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly,alerts&appid=${key}&units=metric`;
-  console.log("url", base + query);
   const { loading, details } = useFetch(base + query);
   let { daily } = details;
-  console.log(details);
+  let toggleComponent = (index) => {
+    setShow(!show);
+    setPosition(index);
+  };
+  let DailyList = () => {
+    return daily.map((element, index) => {
+      //{ dt, description, day, dayNumber, month, max, min }
+      return <DayItem element={element} key={index} />;
+    });
+  };
 
-  let getDailyList = () => {
-    return daily.map(({ temp, dt, weather }) => {
-      let { max, min } = temp;
-      let { description } = weather[0];
-      let date = new Date(dt * 1000);
-      let strDate = date.toString().slice(0, 7).split(" ");
-      let day = strDate[0];
-      let dayNumber = date.getUTCDate();
-      let month = strDate[1];
-      console.log("description", description);
-      return (
-        <section
-          key={dt}
-          className={`daily-card_details ${description.replace(" ", "_")}`}
-        >
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+      <div className="daily-card">
+        <div className={`daily-card_header`}>
+          <h1>Daily report 7 days</h1>
+        </div>
+        {/* {show ? <DailyList /> : <DayDetails daily={daily} index={position} />} */}
+        {DailyList()}
+      </div>
+    </>
+  );
+};
+let DayItem = (props) => {
+  let { temp, weather, dt } = props.element;
+  const [showDetails, setShowDetails] = useState(false);
+  let { max, min } = temp;
+  let { description } = weather[0];
+  let date = new Date(dt * 1000);
+  let strDate = date.toString().slice(0, 7).split(" ");
+  let day = strDate[0];
+  let dayNumber = date.getUTCDate();
+  let month = strDate[1];
+  return (
+    <>
+      <section
+        className={`daily-card_details ${description.replace(" ", "_")}`}
+      >
+        <div className="daily-card_details-header">
           <div className="daily-card_date_wrapper">
             <span>
               {day} {dayNumber < 10 ? "0" + dayNumber : dayNumber} , {month}
@@ -45,26 +72,59 @@ const Daily = ({ city, latitude, longitude }) => {
                 : ""}
             </p>
           </div>
-          <button className="btn">more</button>
-        </section>
-      );
-    });
-  };
-
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
-
-  return (
-    <>
-      <div className="daily-card">
-        <div className={`daily-card_header`}>
-          <h1>Daily report 7 days</h1>
+          <button className="btn" onClick={() => setShowDetails(!showDetails)}>
+            more
+          </button>
         </div>
-        {getDailyList()}
-      </div>
+        {showDetails && <DayDetails element={props.element} />}
+      </section>
     </>
   );
 };
+let DayDetails = ({ element }) => {
+  <div className="daily-card_details-header-item-contet">
+    <div className="panel left">
+      <p>
+        heeey baby
+        <span>watch out</span>
+      </p>
+    </div>
+    <div className="panel">
+      <h1>I im watching tv</h1>
+    </div>
+  </div>;
+  console.log("element", element);
+  let { sunrise, sunset, temp, humidity, pressure, wind_speed } = element;
+  let sunriseDate = new Date(sunrise * 1000),
+    sunriseHour = sunriseDate.getHours(),
+    sunriseMinutes = sunriseDate.getMinutes();
+  let sunsetDate = new Date(sunset * 1000),
+    sunsetHour = sunsetDate.getHours(),
+    sunsetMinutes = sunsetDate.getMinutes();
+  return (
+    <div className="daily-card_details-header-item-contet">
+      <div className="panel left">
+        <p>
+          Sunrise:{" "}
+          <span>
+            {sunriseHour}:
+            {sunriseMinutes < 10 ? "0" + sunriseMinutes : sunriseMinutes} am
+          </span>
+        </p>
+        <p>
+          Sunset:{" "}
+          <span>
+            {sunsetHour}:
+            {sunsetMinutes < 10 ? "0" + sunsetMinutes : sunsetMinutes} am
+          </span>
+        </p>
+      </div>
+      <div className="panel">
+        <h1>I im watching tv</h1>
+      </div>
+    </div>
+  );
 
+  //description daynumber, month, max, min
+};
 export default Daily;
